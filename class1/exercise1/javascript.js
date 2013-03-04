969b919018df9c2a2b9cb010ced999c7d77e69b3
Izzy Johnston <izzycjohnston@gmail.com>
2013-03-04 11:41:59 -0500

    Fixing the year to reflect reality.

--- class1/exercise1/javascript.js
+++ class1/exercise1/javascript.js
@@ -2,6 +2,6 @@ var age = 26;
 var oldAge = 96;
 var perDay = 2;
 
-var days = (oldAge - age) * 356;
+var days = (oldAge - age) * 365;
 var total = perDay * days;
 alert("You will need " + total + " to last you until the ripe old age of " + oldAge);
