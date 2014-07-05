d8d88fd7d91d1ece97df61dfbccd3421c3f08366
Kate Green <kate@kategreen.us>
2014-07-05 12:42:39 -0400

    Edits for the first section of class. Adding more node references, other js uses, and a rules for js slide.

--- class1/exercise1/javascript.js
+++ class1/exercise1/javascript.js
@@ -1,7 +1,24 @@
-var age = 26;
-var oldAge = 96;
-var perDay = 2;
+/**
+ * Variables
+ *
+ * 1. What is your favorite snack? Store this in a variable.
+ * 2. How many snacks do you want to eat per day? Store this in a variable.
+ * 3. Store your age in a variable
+ * 4. Store the age you want to live to in a variable (life expectancy for women is ~81, men is ~77)
+ */
 
-var days = (oldAge - age) * 365;
-var total = perDay * days;
-alert("You will need " + total + " to last you until the ripe old age of " + oldAge);
+
+/**
+ * Calculations
+ *
+ * 1. Make an estimation of how many more days you will live based on these variables. Store this in a variable
+ * 2. How many snacks will you need to have on hand for the rest of your life?
+ */
+
+
+/**
+ * Reporting
+ *
+ * Remember interpolation: 'I need ' + snacks + ' for the rest of my days.'
+ */
+ alert('Put your results here!');
---
+++ class1/exercise1/javscriptAnswer.js
@@ -0,0 +1,7 @@
+var age = 26;
+var oldAge = 96;
+var perDay = 2;
+
+var days = (oldAge - age) * 365;
+var total = perDay * days;
+alert("You will need " + total + " to last you until the ripe old age of " + oldAge);
